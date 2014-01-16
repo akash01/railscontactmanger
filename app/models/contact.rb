918e3cff97c516fed8dcccd4682732de29d743ec
mukesh <pace.akash@gmail.com>
2014-01-16 02:14:20 +0200

    index and layout page added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ActiveRecord::Base
+
+	scope :sorted, lambda {order('contacts.created_at DESC')}
+
+	#validates :first_name, :presence => true,
+	#					:length => { :within => 2..25 }
+
 end
